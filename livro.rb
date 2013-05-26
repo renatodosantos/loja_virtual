276b24b3b2b305b825b7a844e6e159fa5b79438a
renatodosantos <renatos.dosantos@gmail.com>
2013-05-26 00:40:55 -0300

    Usando variaveis de instancia

--- livro.rb
+++ livro.rb
@@ -1,5 +1,8 @@
+# encoding: utf-8
 class Livro
 	def initialize(autor, isbn = "1", numero_de_paginas)
-		puts "Autor: #{autor}, ISBN: #{isbn}, Pág: #{numero_de_paginas}"
+		@autor = autor
+		@isbn = isbn
+		@numero_de_paginas = numero_de_paginas
 	end
 end
