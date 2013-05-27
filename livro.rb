84a3f506e7625c9a8d15ba984bff1e8d6b7b2497
Renato dos Santos <renatos.dosantos@gmail.com>
2013-05-27 17:30:54 -0300

    insercao de modificadores de acesso

--- livro.rb
+++ livro.rb
@@ -1,8 +1,16 @@
 # encoding: utf-8
 class Livro
-	def initialize(autor, isbn = "1", numero_de_paginas)
+	attr_accessor :preco
+
+	def initialize(autor, isbn = "1", numero_de_paginas,preco)
 		@autor = autor
 		@isbn = isbn
 		@numero_de_paginas = numero_de_paginas
+		@preco = preco
 	end
+
+	def to_s
+		"Autor: #{@autor}, Isbn: #{@isbn}, Páginas: #{@numero_de_paginas}, Preco: #{@preco}"
+	end
+
 end
