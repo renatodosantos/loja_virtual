2f06d41053c77a7e7117572683d42364ffd58a1c
renatodosantos <renatos.dosantos@gmail.com>
2013-05-27 21:25:46 -0300

    Usando vetor

---
+++ lib/biblioteca.rb
@@ -0,0 +1,11 @@
+# encoding: utf-8
+class Biblioteca
+	
+	def initialize
+		@livros = []		
+	end
+
+	def adiciona(livro)
+		@livros << livro
+	end
+end
---
+++ lib/livro.rb
@@ -0,0 +1,16 @@
+# encoding: utf-8
+class Livro
+	attr_accessor :preco
+
+	def initialize(autor, isbn = "1", numero_de_paginas,preco)
+		@autor = autor
+		@isbn = isbn
+		@numero_de_paginas = numero_de_paginas
+		@preco = preco
+	end
+
+	def to_s
+		"Autor: #{@autor}, Isbn: #{@isbn}, Páginas: #{@numero_de_paginas}, Preco: #{@preco}"
+	end
+
+end
