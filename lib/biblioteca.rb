e504aa1acb7b9cb2b1bdb8130ca4e3a65f7587eb
renato.santos <renato.santos@synchro.com.br>
2013-08-08 10:01:43 -0300

    usando include

--- lib/biblioteca.rb
+++ lib/biblioteca.rb
@@ -1,33 +1,40 @@
 # encoding: utf-8
 class Biblioteca
 
-	attr_reader :livros
+include Enumerable
 
-	def initialize
-		@banco_de_arquivos = BancoDeArquivos.new
+attr_reader :livros
+
+def initialize
+	@banco_de_arquivos = BancoDeArquivos.new
+end
+
+
+def adiciona(livro)
+	salva livro do
+		livros << livro
 	end
+end
 
+def livros_por_categoria(categoria)
+	livros.select { |livro| livro.categoria == categoria}
+end
 
-	def adiciona(livro)
-		salva livro do
-			livros << livro
-		end
-	end
+def livros
+	@livros ||= @banco_de_arquivos.carrega
+end
 
-	def livros_por_categoria(categoria)
-		livros.select { |livro| livro.categoria == categoria}
-	end
+# método each que possibilita que os outros métodos
+# do módulo Enumerable funcionem em uma instância de Biblioteca
+def each
+	livros.each { |livro| yield livro }
+end
 
+private
 
-	private
-
-	def livros
-		@livros ||= @banco_de_arquivos.carrega
-	end
-
-	def salva (livro)
-		@banco_de_arquivos.salva livro
-		yield
-	end
+def salva (livro)
+	@banco_de_arquivos.salva livro
+	yield
+end
 
 end
